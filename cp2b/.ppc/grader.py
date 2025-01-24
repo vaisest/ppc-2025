481fb049ff97e29d42392094da4b3204ec881267
vaisest <4550061+vaisest@users.noreply.github.com>
2025-01-24 13:15:30 +0200

    initial cp2b (todo: like cp2a)

---
+++ cp2b/.ppc/grader.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from ppcgrader.cli import cli
+import ppccp
+
+if __name__ == "__main__":
+    cli(
+        ppccp.Config(single_precision=False,
+                     openmp=True,
+                     vectorize=False,
+                     code="cp2b"))
