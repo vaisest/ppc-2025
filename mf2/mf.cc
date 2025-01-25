9210dbc8bf21d945fa35e3b3f7b7f1064af95a43
turtvaiz <4550061+vaisest@users.noreply.github.com>
2025-01-25 02:34:12 +0200

    slightly update mf2

--- mf2/mf.cc
+++ mf2/mf.cc
@@ -10,12 +10,14 @@ This is the function you need to implement. Quick reference:
 #include <algorithm>
 #include <utility>
 #include <vector>
+#include <memory>
+#include <cstring>
 void mf(int ny, int nx, int hy, int hx, const float *in, float *out)
 {
 #pragma omp parallel for
   for (int y = 0; y < ny; y++)
   {
-    float *arr = new float[(hx * 2 + 1) * (hy * 2 + 1)]();
+    std::unique_ptr<float[]> arr(new float[(hx * 2 + 1) * (hy * 2 + 1)]);
     const int ystart = std::max(y - hy, 0);
     const int yend = std::min((hy + y + 1), ny);
     const int rows = yend - ystart;
@@ -31,26 +33,22 @@ void mf(int ny, int nx, int hy, int hx, const float *in, float *out)
       {
         const int in_row_idx = (ystart + b) * nx;
         const int arr_row_idx = cols * b;
-        for (int a = 0; a < cols; a++)
-        {
-          arr[a + arr_row_idx] = in[xstart + a + in_row_idx];
-        }
+        std::memcpy(arr.get() + arr_row_idx, in + xstart + in_row_idx, cols * sizeof(float));
       }
 
-      const auto mid = arr + size / 2;
-      const auto rhs = arr + size / 2 - 1;
-      std::nth_element(arr, mid, arr + size);
+      const auto mid = arr.get() + size / 2;
+      const auto rhs = arr.get() + size / 2 - 1;
+      std::nth_element(arr.get(), mid, arr.get() + size);
       out[x + nx * y] = *mid;
       if (size % 2 == 0)
       {
         // save this so the second nth_element doesn't scramble it
 
-        std::nth_element(arr, rhs, arr + size);
+        std::nth_element(arr.get(), rhs, arr.get() + size);
 
         out[x + nx * y] += *rhs;
         out[x + nx * y] /= 2.0;
       }
     }
-    delete[] arr;
   }
 }
