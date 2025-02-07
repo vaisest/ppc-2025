329c2a0a50c2de6ebcf705a62841d8e5638c58ea
turtvaiz <4550061+vaisest@users.noreply.github.com>
2025-02-07 09:01:53 +0200

    finish cp3a and cp3b

---
+++ cp3b/.ppc/ppccp/__init__.py
@@ -0,0 +1,98 @@
+import os
+from typing import Optional
+from ppcgrader.compiler import Compiler
+import ppcgrader.config
+
+
+class Config(ppcgrader.config.Config):
+    def __init__(self,
+                 single_precision: bool,
+                 code: str,
+                 openmp: bool = False,
+                 gpu: bool = False,
+                 vectorize: bool = True):
+        from . import info
+        super().__init__(binary='cp',
+                         cfg_file=__file__,
+                         info=info,
+                         gpu=gpu,
+                         openmp=openmp,
+                         code=code)
+        self.single_precision = single_precision
+        self.vectorize = vectorize
+        self.default_demo = [os.path.join(self.base_dir, 'demo.png')]
+        self.demo_flags = self._demo_flags_png
+        self.demo_post = self._demo_post_png
+
+    def common_flags(self, compiler: Compiler) -> Compiler:
+        compiler = super().common_flags(compiler)
+
+        if self.single_precision:
+            compiler = compiler.add_definition("PPC_ALLOW_FLOAT", 1)
+        else:
+            compiler = compiler.add_definition("PPC_ALLOW_FLOAT", 0)
+
+        if not self.vectorize:
+            compiler = compiler.add_flag("-fno-tree-vectorize")
+
+        return compiler
+
+    def parse_output(self, output):
+        input_data = {
+            "nx": None,
+            "ny": None,
+            "data": None,
+        }
+        output_data = {
+            "result": None,
+        }
+        output_errors = {
+            "max_error": None,
+            "locations": None,
+        }
+        statistics = {}
+
+        def parse_matrix(string):
+            M = string.strip('[]').split(';')
+            M = [row.strip() for row in M]
+            M = [row.split(" ") for row in M]
+            M = [[float(e) for e in row] for row in M]
+            return M
+
+        for line in output.splitlines():
+            splitted = line.split('\t')
+            if splitted[0] == 'result':
+                errors = {
+                    'fail': True,
+                    'pass': False,
+                    'done': False
+                }[splitted[1]]
+            elif splitted[0] == 'time':
+                time = float(splitted[1])
+            elif splitted[0] == 'perf_wall_clock_ns':
+                time = int(splitted[1]) / 1e9
+                statistics[splitted[0]] = int(splitted[1])
+            elif splitted[0].startswith('perf_'):
+                statistics[splitted[0]] = int(splitted[1])
+            elif splitted[0] in [
+                    'max_error', 'max_error_limit', 'gvfa_error',
+                    'gvfa_error_limit'
+            ]:
+                output_errors[splitted[0]] = float(splitted[1])
+            elif splitted[0] in ['ny', 'nx']:
+                input_data[splitted[0]] = int(splitted[1])
+            elif splitted[0] == 'input':
+                input_data["data"] = parse_matrix(splitted[1])
+            elif splitted[0] == 'output':
+                output_data["result"] = parse_matrix(splitted[1])
+            elif splitted[0] == 'locations':
+                output_errors["locations"] = parse_matrix(splitted[1])
+
+        nx = input_data.get('nx', None)
+        ny = input_data.get('ny', None)
+        if nx and ny and self.code != "cp9a":
+            flops = ny * ny * nx + 4 * ny * nx
+            statistics['operations'] = flops
+            statistics['operations_name'] = "useful arithmetic operation"
+
+        return time, errors, input_data, output_data, output_errors, statistics
--- cp3b/cp.cc
+++ cp3b/cp.cc
@@ -11,23 +11,23 @@ This is the function you need to implement. Quick reference:
 - correlation between rows i and row j has to be stored in result[i + j*ny]
 - only parts with 0 <= j <= i < ny need to be filled
 */
-constexpr int LANES = 8;
-typedef double f64x8 __attribute__((vector_size(LANES * sizeof(double))));
-constexpr f64x8 zero_vec = {};
+constexpr int LANES = 16;
+typedef float f32x16 __attribute__((vector_size(LANES * sizeof(float))));
+constexpr f32x16 zero_vec = {};
 
-double vec_sum(f64x8 v)
+double vec_sum(f32x16 v)
 {
 #ifdef __AVX512DQ__
-    return _mm512_reduce_add_pd(v);
+    return _mm512_reduce_add_ps(v);
 #else
-    return ((v[0] + v[1]) + (v[2] + v[3])) + ((v[4] + v[5]) + (v[6] + v[7]));
+    return ((v[0] + v[1]) + (v[2] + v[3])) + ((v[4] + v[5]) + (v[6] + v[7])) + ((v[8] + v[9]) + (v[10] + v[11])) + ((v[12] + v[13]) + (v[14] + v[15]));
 #endif
 }
 
-inline f64x8 vec_fmadd(f64x8 a, f64x8 b, f64x8 c)
+inline f32x16 vec_fmadd(f32x16 a, f32x16 b, f32x16 c)
 {
 #ifdef __AVX512DQ__
-    return _mm512_fmadd_pd(a, b, c);
+    return _mm512_fmadd_ps(a, b, c);
 #else
     // this might get compiled to the above anyway on avx512, but idk let's be
     // sure
@@ -43,18 +43,17 @@ void correlate(const int ny, const int nx, const float *data, float *result)
     constexpr int N = 12;
     // height padding multiplier
     constexpr int M = 14;
-    // 12x14 seems to be optimal for the test server
 
     row_blocks = row_blocks + (N - (row_blocks % N));
     const int rows = ny + (M - (ny % M));
-    std::unique_ptr<f64x8[]> norm(new f64x8[row_blocks * rows]);
+    std::unique_ptr<f32x16[]> norm(new f32x16[row_blocks * rows]);
     // approach here is as suggested in the tip
 
 #pragma omp parallel for
     for (int y = 0; y < rows; y++)
     {
-        double sum = 0;
-        f64x8 arr = zero_vec;
+        float sum = 0;
+        f32x16 arr = zero_vec;
 
         int counter = 0;
         if (y >= ny)
@@ -70,7 +69,7 @@ void correlate(const int ny, const int nx, const float *data, float *result)
             for (int v = 0; v < LANES; v++)
             {
                 int source_x = block * LANES + v;
-                const double value = source_x >= nx ? 0.0 : data[source_x + y * nx];
+                const float value = source_x >= nx ? 0.0 : data[source_x + y * nx];
                 arr[counter] = value;
                 sum += value;
                 counter++;
@@ -92,8 +91,8 @@ void correlate(const int ny, const int nx, const float *data, float *result)
             norm[y * row_blocks + row_blocks - 1] = arr;
         }
 
-        double mean = sum / (double)nx;
-        double sum_sq = 0;
+        float mean = sum / (float)nx;
+        float sum_sq = 0;
         for (int block = 0; block < row_blocks; block++)
         {
             const int x = block * LANES;
@@ -112,11 +111,11 @@ void correlate(const int ny, const int nx, const float *data, float *result)
             {
                 norm[block_idx] -= mean;
             }
-            f64x8 square = norm[block_idx] * norm[block_idx];
+            f32x16 square = norm[block_idx] * norm[block_idx];
             sum_sq += vec_sum(square);
         }
 
-        double sq_sqrt = sqrt(sum_sq);
+        float sq_sqrt = sqrt(sum_sq);
         for (int x = 0; x < nx; x += LANES)
         {
             int block_idx = y * row_blocks + x / LANES;
@@ -130,7 +129,7 @@ void correlate(const int ny, const int nx, const float *data, float *result)
         for (int y = 0; y <= x; y += M)
         {
             // we calculate MxM row correlations at a time
-            f64x8 stuff[M][M] = {};
+            f32x16 stuff[M][M] = {};
 
             // avoid calculating unnecessary correlations
             const int iM = M > ny - y ? ny - y : M;
@@ -140,18 +139,18 @@ void correlate(const int ny, const int nx, const float *data, float *result)
             {
                 for (int i = 0; i < iM; i++)
                 {
-                    f64x8 y0 = norm[(y + i) * row_blocks + k];
-                    f64x8 y1 = norm[(y + i) * row_blocks + k + 1];
-                    f64x8 y2 = norm[(y + i) * row_blocks + k + 2];
-                    f64x8 y3 = norm[(y + i) * row_blocks + k + 3];
-                    f64x8 y4 = norm[(y + i) * row_blocks + k + 4];
-                    f64x8 y5 = norm[(y + i) * row_blocks + k + 5];
-                    f64x8 y6 = norm[(y + i) * row_blocks + k + 6];
-                    f64x8 y7 = norm[(y + i) * row_blocks + k + 7];
-                    f64x8 y8 = norm[(y + i) * row_blocks + k + 8];
-                    f64x8 y9 = norm[(y + i) * row_blocks + k + 9];
-                    f64x8 y10 = norm[(y + i) * row_blocks + k + 10];
-                    f64x8 y11 = norm[(y + i) * row_blocks + k + 11];
+                    f32x16 y0 = norm[(y + i) * row_blocks + k];
+                    f32x16 y1 = norm[(y + i) * row_blocks + k + 1];
+                    f32x16 y2 = norm[(y + i) * row_blocks + k + 2];
+                    f32x16 y3 = norm[(y + i) * row_blocks + k + 3];
+                    f32x16 y4 = norm[(y + i) * row_blocks + k + 4];
+                    f32x16 y5 = norm[(y + i) * row_blocks + k + 5];
+                    f32x16 y6 = norm[(y + i) * row_blocks + k + 6];
+                    f32x16 y7 = norm[(y + i) * row_blocks + k + 7];
+                    f32x16 y8 = norm[(y + i) * row_blocks + k + 8];
+                    f32x16 y9 = norm[(y + i) * row_blocks + k + 9];
+                    f32x16 y10 = norm[(y + i) * row_blocks + k + 10];
+                    f32x16 y11 = norm[(y + i) * row_blocks + k + 11];
                     // f64x8 y12 = norm[(y + i) * row_blocks + k + 12];
                     // f64x8 y13 = norm[(y + i) * row_blocks + k + 13];
                     // f64x8 y14 = norm[(y + i) * row_blocks + k + 14];
@@ -160,18 +159,18 @@ void correlate(const int ny, const int nx, const float *data, float *result)
                     // f64x8 y17 = norm[(y + i) * row_blocks + k + 17];
                     for (int j = 0; j < jM; j++)
                     {
-                        f64x8 x0 = norm[(x + j) * row_blocks + k];
-                        f64x8 x1 = norm[(x + j) * row_blocks + k + 1];
-                        f64x8 x2 = norm[(x + j) * row_blocks + k + 2];
-                        f64x8 x3 = norm[(x + j) * row_blocks + k + 3];
-                        f64x8 x4 = norm[(x + j) * row_blocks + k + 4];
-                        f64x8 x5 = norm[(x + j) * row_blocks + k + 5];
-                        f64x8 x6 = norm[(x + j) * row_blocks + k + 6];
-                        f64x8 x7 = norm[(x + j) * row_blocks + k + 7];
-                        f64x8 x8 = norm[(x + j) * row_blocks + k + 8];
-                        f64x8 x9 = norm[(x + j) * row_blocks + k + 9];
-                        f64x8 x10 = norm[(x + j) * row_blocks + k + 10];
-                        f64x8 x11 = norm[(x + j) * row_blocks + k + 11];
+                        f32x16 x0 = norm[(x + j) * row_blocks + k];
+                        f32x16 x1 = norm[(x + j) * row_blocks + k + 1];
+                        f32x16 x2 = norm[(x + j) * row_blocks + k + 2];
+                        f32x16 x3 = norm[(x + j) * row_blocks + k + 3];
+                        f32x16 x4 = norm[(x + j) * row_blocks + k + 4];
+                        f32x16 x5 = norm[(x + j) * row_blocks + k + 5];
+                        f32x16 x6 = norm[(x + j) * row_blocks + k + 6];
+                        f32x16 x7 = norm[(x + j) * row_blocks + k + 7];
+                        f32x16 x8 = norm[(x + j) * row_blocks + k + 8];
+                        f32x16 x9 = norm[(x + j) * row_blocks + k + 9];
+                        f32x16 x10 = norm[(x + j) * row_blocks + k + 10];
+                        f32x16 x11 = norm[(x + j) * row_blocks + k + 11];
                         // f64x8 x12 = norm[(x + j) * row_blocks + k + 12];
                         // f64x8 x13 = norm[(x + j) * row_blocks + k + 13];
                         // f64x8 x14 = norm[(x + j) * row_blocks + k + 14];
@@ -210,7 +209,7 @@ void correlate(const int ny, const int nx, const float *data, float *result)
             {
                 for (int j = 0; j < jM; j++)
                 {
-                    result[(y + i) * ny + x + j] = (float)vec_sum(stuff[i][j]);
+                    result[(y + i) * ny + x + j] = vec_sum(stuff[i][j]);
                 }
             }
         }
